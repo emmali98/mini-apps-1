3e600dcb3e7819b7c5a33f3a1739c3f6959458d6
Emma Li <emma.m.li.98@gmail.com>
2020-09-18 14:07:49 -0700

    Implements basic game functionality, reset button, winner alert

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,39 +1,98 @@
 // Model
-var board = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
 
-var turn = {
-  true: '|X|',
-  false: '|O|',
-  whose: true
-};
+var spaces = document.getElementsByClassName('space');
+var rows = document.getElementsByClassName('row');
+
+var board = [];
+var turn = {};
+
 
 // Controllers
+
 var makeMove = function(e) {
-  console.log(e.target.id);
+  var coords = [Number(e.target.id[0]), Number(e.target.id[1])];
+  var move = turn.whose ? 1 : -1;
+  board[coords[0]][coords[1]] = move;
+}
+
+var _statusCheck = function() {
+  for (var i = 0; i < 3; i++) {
+    var rowSum = board[i][0] + board[i][1] + board[i][2];
+    var colSum = board[0][i] + board[1][i] + board[2][i];
+    var diagSum = board[0][i] + board[1][1] + board[2][2 - i];
+
+    if (rowSum === 3 || colSum === 3 || diagSum === 3) {
+      return 'X wins';
+    } else if (rowSum === -3 || colSum === -3 || diagSum === -3) {
+      return 'O wins';
+    }
+  }
+
+  if (!board.flat().includes(null)) {
+    return 'It\'s a tie';
+  }
+
+  return null;
+}
+
+var checkForEnd = function(e) {
+  var status = _statusCheck();
+  if (status) {
+    document.getElementById('winner').innerHTML = status;
+  }
+}
+
+var resetBoard = function() {
+  board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+}
+
+var resetTurn = function() {
+  turn = {
+    true: '|X|',
+    false: '|O|',
+    whose: true
+  };
 }
 
 // Views
+
 var toggleSpace = function(e) {
   e.target.innerHTML = turn[turn.whose];
   turn.whose = !turn.whose;
 }
 
-
-// Structuring
-var spaces = document.getElementsByClassName('space');
-var rows = document.getElementsByClassName('row');
-
-for (var i = 0; i < spaces.length; i++) {
-  spaces[i].style.display = "inline-block";
+var resetView = function() {
+  for (var i = 0; i < spaces.length; i++) {
+    spaces[i].style.display = "inline-block";
+    spaces[i].innerHTML = '|_|';
+  }
+  document.getElementById('winner').innerHTML = '';
 }
+
+
+// Initialize
+
+var reset = function() {
+  resetBoard();
+  resetView();
+  resetTurn();
+}
+
 for (var i = 0; i < rows.length; i++) {
   rows[i].style.display = "block";
+
   rows[i].onclick = function(e) {
-    makeMove(e);
-    toggleSpace(e);
+    if (e.target.innerHTML === '|_|') {
+      makeMove(e);
+      toggleSpace(e);
+    }
   }
 }
+
+document.onclick = checkForEnd;
+document.getElementById('reset').onclick = reset;
+reset();

dcd17b2ab431b44fd69cb146be65fc82cd0e70ab
Emma Li <emma.m.li.98@gmail.com>
2020-09-19 15:59:20 -0700

    Implements client side (no jQuery yet)

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,11 @@
+const form = document.getElementById('form');
+const input = document.getElementById('json');
 
+// Handling file submission
+var handleSubmit = (e) => {
+  e.preventDefault();
+}
+form.onsubmit = handleSubmit;
 
+// Formatting CSV
+document.getElementById('crv').style.whiteSpace = 'pre-wrap';
